7e250fe756cf60009ccd574f8899604958c790a8
41407 <jimi.welling@cs.helsinki.fi>
2015-09-15 13:00:30 +0300

    Spawnable enemies

--- Assets/Scripts/Factory.cs
+++ Assets/Scripts/Factory.cs
@@ -4,12 +4,7 @@ using System.Collections;
 public class Factory : MonoBehaviour
 {
 	public GameObject player;
-	public GameObject playerBullet;
 	public GameObject enemy;
-	public GameObject spawner;
-	public GameObject machineGun;
-	public GameObject shotgun;
-	public GameObject revolver;
 
 	//Here is a private reference only this class can access
 	private static Factory _instance;
@@ -36,34 +31,10 @@ public class Factory : MonoBehaviour
 	{
 		return InitializeParameters (ObjectPool.pool.Pull (gameObject, position, rotation));
 	}
-	
-	public GameObject PlayerBullet (Vector2 position, Quaternion rotation)
-	{
-		return ByReference (playerBullet, position, rotation);
-	}
+
 	
 	public GameObject Enemy (Vector2 position, Quaternion rotation)
 	{
 		return ByReference (enemy, position, rotation);
-	}
-	
-	public GameObject Spawner (Vector2 position, Quaternion rotation)
-	{
-		return ByReference (spawner, position, rotation);
-	}
-
-	public GameObject Revolver (Vector2 position, Quaternion rotation)
-	{
-		return ByReference (revolver, position, rotation);
-	}
-
-	public GameObject MachineGun (Vector2 position, Quaternion rotation)
-	{
-		return ByReference (machineGun, position, rotation);
-	}
-
-	public GameObject Shotgun (Vector2 position, Quaternion rotation)
-	{
-		return ByReference (shotgun, position, rotation);
 	}
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			Factory.create.Enemy (new Vector2 (Random.Range (-10, 10), Random.Range (-10, 10)), Quaternion.identity);
+		}
+	}
+}
