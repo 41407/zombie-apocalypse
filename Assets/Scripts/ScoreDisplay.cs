897d3bd9edd141ac043402dcab08949d2ace5cd3
41407 <jimi.welling@cs.helsinki.fi>
2015-09-15 16:25:12 +0300

    Scoring, fonts

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreDisplay : MonoBehaviour
+{
+	private Text text;
+
+	void OnEnable ()
+	{
+		text = GetComponent<Text> ();
+	}
+
+	void Update ()
+	{
+		text.text = "" + Score.score;
+	}
+}
---
+++ Assets/Scripts/Scoring.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scoring : MonoBehaviour
+{
+		public int scoreOnDeath = 0;
+
+		void ApplyScoring ()
+		{
+				Score.AddScore (scoreOnDeath);
+		}
+}
