ba8ee96cdbedfd4e298251eb9583c26e950034d3
41407 <jimi.welling@cs.helsinki.fi>
2015-09-15 13:18:58 +0300

    Let player aim

---
+++ Assets/Scripts/LookAtObject.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtObject : MonoBehaviour
+{
+	public GameObject objectToLookAt;
+	public bool continuous = true;
+
+	void OnEnable ()
+	{
+		if (objectToLookAt) {
+			TurnTowardsObject ();
+		}
+	}
+
+	void Update ()
+	{
+		if (objectToLookAt && continuous) {
+			TurnTowardsObject ();
+		}
+	}
+
+	private void TurnTowardsObject ()
+	{
+		Vector3 relativeLookDirection = gameObject.transform.position - objectToLookAt.transform.position;
+		float angle = Mathf.Atan2 (relativeLookDirection.y, relativeLookDirection.x) * Mathf.Rad2Deg + 90;
+		transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+	}
+}
---
+++ Assets/Scripts/Mouse.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mouse : MonoBehaviour {
+
+	void Update () {
+		Vector2 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		transform.position = newPosition;
+	}
+}
