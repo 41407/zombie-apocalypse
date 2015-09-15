537662acab869437d2d8adfe39cfa3624817d343
41407 <jimi.welling@cs.helsinki.fi>
2015-09-15 13:39:58 +0300

    Better shooting

---
+++ Assets/Scripts/OutOfLOSDecay.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutOfLOSDecay : MonoBehaviour
+{
+	public float decayTime = 0.0f;
+	public float decayTimer;
+	public bool outOfLOS;
+	
+	void OnEnable ()
+	{
+		decayTimer = decayTime;
+	}
+
+	void Update ()
+	{
+		if (outOfLOS) {
+			if (decayTime > 0f) {
+				decayTimer -= Time.deltaTime / decayTime;
+			}
+			if (decayTimer <= 0f || decayTime == 0f) {
+				gameObject.SetActive (false);
+			}
+		}
+	}
+	
+	void OnBecameVisible ()
+	{
+		outOfLOS = false;
+	}
+
+	void OnBecameInvisible ()
+	{
+		outOfLOS = true;
+	}
+}
---
+++ Assets/Scripts/TrailDecay.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrailDecay : MonoBehaviour
+{
+	/**
+	 * 
+	 *  Source: 
+	 * 	http://forum.unity3d.com/threads/trailrenderer-reset.38927/
+	 */
+
+
+	protected TrailRenderer trail;
+	protected float decayTime = 0;
+		
+	void Awake ()
+	{
+		trail = gameObject.GetComponent<TrailRenderer> ();
+		if (!trail) {
+			Debug.LogError ("[TrailRendererHelper.Awake] invalid TrailRenderer.");
+			return;
+		}
+		decayTime = trail.time;
+	}
+		
+	void OnEnable ()
+	{
+		if (!trail) {
+			return;
+		}
+		StartCoroutine (ResetTrails ());
+	}
+		
+	IEnumerator ResetTrails ()
+	{
+		trail.time = 0;
+		yield return new WaitForEndOfFrame ();
+		trail.time = decayTime;
+	}
+}
+
