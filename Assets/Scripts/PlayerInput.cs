5688fa531480a70c3f48ceeff4cfa6d52769b8ff
41407 <jimi.welling@cs.helsinki.fi>
2015-09-15 12:43:56 +0300

    Movable player

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour
+{
+	public float acceleration;
+
+	void Update ()
+	{
+		Movement ();
+		// Firing ();
+	}
+
+	void Movement ()
+	{	
+		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		gameObject.GetComponent<Rigidbody2D> ().AddForce (input * acceleration);
+	}
+
+	void Firing ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			gameObject.SendMessage ("StartFiring");
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			gameObject.SendMessage ("StopFiring");
+		}
+	}
+}
