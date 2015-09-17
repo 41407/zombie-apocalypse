28ece0b33eb2040bfde74b54b9efc12518be09dc
41407 <jimi.welling@cs.helsinki.fi>
2015-09-17 13:07:29 +0300

    Add highscore scene and transitions

--- Assets/Scripts/Game Controller/GameController.cs
+++ Assets/Scripts/Game Controller/GameController.cs
@@ -4,10 +4,29 @@ using System.Collections;
 public class GameController : MonoBehaviour
 {
 
+	public GameObject player;
+	public float nextLevelTimeout;
+	public string nextLevel;
+	private bool playerIsAlive = true;
+
 	void Update ()
 	{
+		if(!player && playerIsAlive) {
+			Invoke("PlayerDied", 0);
+			Invoke("EndGame", nextLevelTimeout);
+			playerIsAlive = false;
+		}
 		if (Input.GetKeyDown (KeyCode.Alpha1)) {
 			Factory.create.Enemy (new Vector2 (Random.Range (-10, 10), Random.Range (-10, 10)), Quaternion.identity);
 		}
+	}
+
+	private void PlayerDied() {
+		print("Player died!");
+	}
+
+	private void EndGame() {
+		print("Next scene.");
+		Application.LoadLevel(nextLevel);
 	}
 }
