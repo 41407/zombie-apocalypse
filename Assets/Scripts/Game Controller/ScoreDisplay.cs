49ef9fb872643f29640e887c8b15c04b2540114c
41407 <jimi.welling@cs.helsinki.fi>
2015-09-19 22:15:45 +0300

    HUD to display score

---
+++ Assets/Scripts/Game Controller/ScoreDisplay.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreDisplay : MonoBehaviour
+{
+	public string staticText = "";
+
+	void Update ()
+	{
+		GetComponent<Text> ().text = staticText + " " + Score.GetScore ();
+	}
+}
