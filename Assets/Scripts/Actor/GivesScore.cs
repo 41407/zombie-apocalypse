1966fda7fa2768bb5d34ab69cae08545ace62b74
41407 <jimi.welling@cs.helsinki.fi>
2015-09-19 22:10:00 +0300

    Add scoring

---
+++ Assets/Scripts/Actor/GivesScore.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GivesScore : MonoBehaviour
+{
+		public int scoreOnDeath = 0;
+
+		void ApplyScoring ()
+		{
+				Score.AddScore (scoreOnDeath);
+		}
+}
---
+++ Assets/Scripts/Game Controller/Score.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Score
+{
+		public static int score = 0;
+
+		public static void Reset ()
+		{
+				score = 0;
+		}
+
+		public static void AddScore ()
+		{
+				AddScore (1);
+		}
+
+		public static void AddScore (int amount)
+		{
+				score += amount;
+		}
+
+		public static int GetScore ()
+		{
+				return score;
+		}
+}
