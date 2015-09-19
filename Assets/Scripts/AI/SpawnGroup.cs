e772aeaaeec6e832432826fc5123bbead3f07d13
41407 <jimi.welling@cs.helsinki.fi>
2015-09-20 00:07:32 +0300

    Add spawn group

---
+++ Assets/Scripts/AI/SpawnGroup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnGroup : MonoBehaviour
+{
+	public int numberOfEnemies = 0;
+
+	void Update() {
+		Factory.create.Enemy(transform.position, Quaternion.identity);
+		numberOfEnemies--;
+		if (numberOfEnemies < 0) {
+			gameObject.SetActive(false);
+		}
+	}
+
+	public void Spawn(int numberOfEnemies) {
+		this.numberOfEnemies = numberOfEnemies;
+	}
+}
---
+++ Assets/Scripts/Game Controller/EnemySpawner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+	public GameObject enemyGroup;
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			GameObject group = Factory.create.ByReference(enemyGroup, Vector2.zero, Quaternion.identity);
+			group.SendMessage("Spawn", 10);
+		}
+	}
+}
