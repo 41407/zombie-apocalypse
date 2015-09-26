9af3b8aee36a3a20faed4186479583a9f651fdf3
41407 <jimi.welling@cs.helsinki.fi>
2015-09-26 15:48:45 +0300

    Tweaks, more even distribution of enemies

--- Assets/Scripts/Game Controller/EnemySpawner.cs
+++ Assets/Scripts/Game Controller/EnemySpawner.cs
@@ -6,9 +6,10 @@ public class EnemySpawner : MonoBehaviour
 
 	public GameObject enemyGroup;
 	public int hordeSize;
-	public float spawnInterval = 2.0f;
-	public float distanceFromPlayer = 5.0f;
-	private Vector2 playerPosition;
+	public float spawnInterval = 0.5f;
+	public float minimumDistanceFromPlayer = 5.0f;
+	public float distanceVariance = 5.0f;
+	private Vector2 cameraPosition;
 
 	void OnEnable ()
 	{
@@ -18,36 +19,50 @@ public class EnemySpawner : MonoBehaviour
 	public void Spawn ()
 	{
 		if (GetComponent<SceneController> ().player) {
-			playerPosition = GetComponent<SceneController> ().player.transform.position;
-			int formation = Random.Range (0, 1);
+			cameraPosition = Camera.main.transform.position;
+			int formation = Random.Range (0, 10);
 			switch (formation) {
 			case 0:
 				Horde ();
 				break;
 			case 1:
-				Surround ();
+				Surround (Random.Range (10, 50), 5);
 				break;
 			default:
+				Surround (Random.Range (1, 3), 1);
 				break;
 			}
 		}
 	}
 
-	public void Surround ()
+	public void Surround (int numberOfGroups, int enemiesInGroup)
 	{
 		Vector2 groupPosition;
-		for (int i = 0; i < Random.Range(10, 50); i++) {
-			groupPosition = Random.insideUnitCircle.normalized * distanceFromPlayer + playerPosition;
-			GameObject group = Factory.create.ByReference (enemyGroup, groupPosition, Quaternion.identity);
-			group.SendMessage ("Spawn", Random.Range (1, 5));
+		for (int i = 0; i < numberOfGroups; i++) {
+			groupPosition = RandomDirection () * Distance () + cameraPosition;
+			for (int j = 0; j < Random.Range (1, enemiesInGroup); j++) {
+				Factory.create.Enemy (RandomDirection () * Random.Range (0, 2.0f) + groupPosition, Quaternion.identity);
+			}
 		}
 	}
 
 	public void Horde ()
 	{
-		Vector2 groupPosition = Random.insideUnitCircle.normalized * distanceFromPlayer + playerPosition;
+		Vector2 groupPosition = RandomDirection () * Distance () + cameraPosition;
 		GameObject group = Factory.create.ByReference (enemyGroup, groupPosition, Quaternion.identity);
-		group.SendMessage ("Spawn", hordeSize);
+		for (int j = 0; j < hordeSize; j++) {
+			Factory.create.Enemy (RandomDirection () * Random.Range (0, 2.0f) + groupPosition, Quaternion.identity);
+		}
 		hordeSize++;
+	}
+
+	static Vector2 RandomDirection ()
+	{
+		return Random.insideUnitCircle.normalized;
+	}
+
+	private float Distance ()
+	{
+		return minimumDistanceFromPlayer + Random.Range (0, distanceVariance);
 	}
 }
