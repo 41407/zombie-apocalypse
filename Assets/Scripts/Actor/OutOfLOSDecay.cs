09f661a174afcc7df21799ca5910a5d8bf0e7aab
41407 <jimi.welling@cs.helsinki.fi>
2015-09-28 14:20:21 +0300

    Refactor OutOfLOSDecay to use Invoke

--- Assets/Scripts/Actor/OutOfLOSDecay.cs
+++ Assets/Scripts/Actor/OutOfLOSDecay.cs
@@ -4,33 +4,19 @@ using System.Collections;
 public class OutOfLOSDecay : MonoBehaviour
 {
 	public float decayTime = 0.0f;
-	public float decayTimer;
-	public bool outOfLOS;
-	
-	void OnEnable ()
-	{
-		decayTimer = decayTime;
-	}
 
-	void Update ()
-	{
-		if (outOfLOS) {
-			if (decayTime > 0f) {
-				decayTimer -= Time.deltaTime / decayTime;
-			}
-			if (decayTimer <= 0f || decayTime == 0f) {
-				gameObject.SetActive (false);
-			}
-		}
-	}
-	
 	void OnBecameVisible ()
 	{
-		outOfLOS = false;
+		CancelInvoke();
 	}
 
 	void OnBecameInvisible ()
 	{
-		outOfLOS = true;
+		Invoke("Destroy", decayTime);
+	}
+
+	private void Destroy() 
+	{
+		gameObject.SetActive (false);
 	}
 }
