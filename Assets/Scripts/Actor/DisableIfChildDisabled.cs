fff1705528d54146879368fc358c43896f12157c
41407 <jimi.welling@cs.helsinki.fi>
2015-09-29 19:30:50 +0300

    Start stalkingEnemy

---
+++ Assets/Scripts/Actor/DisableIfChildDisabled.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableIfChildDisabled : MonoBehaviour
+{
+
+	public GameObject child;
+
+	void OnEnable ()
+	{
+		if (!child) {
+			child = transform.GetChild (0).gameObject;
+		}
+		child.SetActive (true);
+	}
+
+	void Update ()
+	{
+		if (!child.activeSelf) {
+			gameObject.SetActive (false);
+		}
+	}
+}
