ca9d2250415123267946ed030a2247d7e8c41d9a
41407 <jimi.welling@cs.helsinki.fi>
2015-09-29 19:01:18 +0300

    Make enemy types look distinct, persp camera

--- Assets/Scripts/Player/Mouse.cs
+++ Assets/Scripts/Player/Mouse.cs
@@ -14,7 +14,21 @@ public class Mouse : MonoBehaviour
 
 	void Update ()
 	{
-		Vector2 newPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-		transform.position = newPosition;
+		transform.position = GetWorldPositionOnPlane (Input.mousePosition, 0);
+	}
+
+	/**
+		 *  Uses raycasting to find given screen position on a plane perpendicular to the camera
+		 *  in given z coordinate;
+		 * 
+		 *  http://answers.unity3d.com/questions/566519/camerascreentoworldpoint-in-perspective.html
+		 */
+	private Vector3 GetWorldPositionOnPlane (Vector3 screenPosition, float z)
+	{
+		Ray ray = Camera.main.ScreenPointToRay (screenPosition);
+		Plane xy = new Plane (Vector3.forward, new Vector3 (0, 0, z));
+		float distance;
+		xy.Raycast (ray, out distance);
+		return ray.GetPoint (distance);
 	}
 }
