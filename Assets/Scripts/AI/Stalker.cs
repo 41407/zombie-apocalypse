632302ef8a17ec02b9f02ea97217d53e89706ad6
41407 <jimi.welling@cs.helsinki.fi>
2015-09-29 19:40:31 +0300

    Add stalking enemy

---
+++ Assets/Scripts/AI/Stalker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stalker : MonoBehaviour
+{
+	public float chargeSpeed;
+	public bool onCooldown = false;
+	public float cooldownTime = 2.0f;
+
+	void OnTriggerStay2D (Collider2D col)
+	{
+		if (!onCooldown) {
+			gameObject.GetComponent<Rigidbody2D> ().AddForce (transform.rotation * Vector2.up * chargeSpeed);
+			onCooldown = true;
+			Invoke ("CancelCooldown", cooldownTime);
+		}
+	}
+
+	private void CancelCooldown ()
+	{
+		onCooldown = false;
+	}
+}
