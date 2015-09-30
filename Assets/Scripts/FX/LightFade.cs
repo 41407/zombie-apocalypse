2aa64ddf5c86fd5f278d02927703c19a379c943f
41407 <jimi.welling@cs.helsinki.fi>
2015-09-30 08:38:26 +0300

    Beautifuler explosions

---
+++ Assets/Scripts/FX/LightFade.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFade : MonoBehaviour
+{
+	private Light light;
+	public float fadeSpeed;
+
+	void OnEnable ()
+	{
+		light = gameObject.GetComponent<Light> ();
+		light.intensity = 1.00f;
+	}
+
+	void Update ()
+	{
+		light.intensity = light.intensity - Time.deltaTime * fadeSpeed;
+	}
+}
