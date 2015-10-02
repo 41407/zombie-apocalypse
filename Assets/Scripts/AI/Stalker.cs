1160d72d0e6694746d36105c44cccd9b04a8f0c6
41407 <jimi.welling@cs.helsinki.fi>
2015-10-02 18:23:12 +0300

    Make stalkers more accurate

--- Assets/Scripts/AI/Stalker.cs
+++ Assets/Scripts/AI/Stalker.cs
@@ -11,11 +11,24 @@ public class Stalker : MonoBehaviour
 	void OnTriggerEnter2D (Collider2D col)
 	{
 		if (!onCooldown && targetTag.Contains (col.tag)) {
-			gameObject.GetComponent<Rigidbody2D> ().AddForce (transform.rotation * Vector2.up * chargeSpeed);
-			gameObject.GetComponent<TrailRenderer> ().enabled = true;
+			Turn (col.gameObject);
+			Charge ();
 			onCooldown = true;
 			Invoke ("CancelCooldown", cooldownTime);
 		}
+	}
+
+	private void Turn (GameObject towards)
+	{
+		Vector3 relativeLookDirection = transform.position - towards.transform.position;
+		float angle = Mathf.Atan2 (relativeLookDirection.y, relativeLookDirection.x) * Mathf.Rad2Deg + 90;
+		transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+	}
+
+	void Charge ()
+	{
+		gameObject.GetComponent<Rigidbody2D> ().AddForce (transform.rotation * Vector2.up * chargeSpeed);
+		gameObject.GetComponent<TrailRenderer> ().enabled = true;
 	}
 
 	private void CancelCooldown ()
