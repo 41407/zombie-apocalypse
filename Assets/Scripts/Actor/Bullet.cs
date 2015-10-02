20cbe2e540264e81710dd2047e56216ff66e8883
41407 <jimi.welling@cs.helsinki.fi>
2015-10-02 09:32:09 +0300

    Player bullet to use particle as trail

--- Assets/Scripts/Actor/Bullet.cs
+++ Assets/Scripts/Actor/Bullet.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour
 {
-
 	public float speed = 500.0f;
+	public GameObject trailParticle;
 
 	void OnEnable ()
 	{
 		GetComponent<Rigidbody2D> ().AddForce (transform.rotation * Vector3.up * speed);
+	}
+
+	void Update ()
+	{
+		if (trailParticle) {
+			Factory.create.ByReference (trailParticle, transform.position, transform.rotation);
+		}
 	}
 
 	public void SetVelocity (Vector2 velocity)
