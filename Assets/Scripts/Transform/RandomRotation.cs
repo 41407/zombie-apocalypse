5183ff55346587ec35beab38e1f692002977fcad
41407 <jimi.welling@cs.helsinki.fi>
2015-10-02 09:42:54 +0300

    Bullet texture, random xy rotation

---
+++ Assets/Scripts/Transform/RandomRotation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotation : MonoBehaviour
+{
+	public bool x;
+	public bool y;
+	public bool z;
+
+	void OnEnable ()
+	{
+		if (x) {
+			transform.Rotate (Vector3.right, Random.Range (0, 360));
+		}
+		if (y) {
+			transform.Rotate (Vector3.up, Random.Range (0, 360));
+		}
+		if (z) {
+			transform.Rotate (Vector3.forward, Random.Range (0, 360));
+		}
+	}
+}
