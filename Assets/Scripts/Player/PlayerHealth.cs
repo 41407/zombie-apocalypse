e7448991c3dc207dec7c47cbc19d1e6b46a29244
41407 <jimi.welling@cs.helsinki.fi>
2015-10-03 00:04:09 +0300

    Real health system for player

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+	public float maxHealth = 3.0f;
+	public float currentHealth;
+	public float regenRate = 0.5f;
+	public bool regenerate = true;
+	public bool screenShakeOnDamage = true;
+	public GameObject explosion;
+	public GameObject tookDamage;
+
+	void OnEnable ()
+	{
+		currentHealth = maxHealth;
+	}
+	
+	void Update ()
+	{
+		if (regenerate && currentHealth < maxHealth) {
+			currentHealth = Mathf.Clamp (currentHealth + regenRate * Time.deltaTime, 0, maxHealth);
+		}
+	}
+
+	void StartFiring ()
+	{
+		regenerate = false;
+	}
+
+	void StopFiring ()
+	{
+		regenerate = true;
+	}
+
+	void TakeDamage (float damage)
+	{
+		currentHealth -= damage;
+		Camera.main.SendMessage ("Shake", 2.0f);
+		if (tookDamage) {
+			Factory.create.ByReference (tookDamage, transform.position, transform.rotation);
+		}
+		if (currentHealth < 0) {
+			if (explosion) {
+				Factory.create.ByReference (explosion, transform.position, transform.rotation);
+			}
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerHealthParticles.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealthParticles : MonoBehaviour
+{
+	private ParticleSystem particles;
+	private PlayerHealth health;
+	private Color flameColor;
+	public float woundedSize = 0.5f;
+	public float fullHealthSize = 2.0f;
+	public float woundedRate = 5;
+	public float fullHealthRate = 20;
+
+	void OnEnable ()
+	{
+		flameColor = new Color (1, 1, 0);
+		particles = gameObject.GetComponent<ParticleSystem> ();
+		health = transform.parent.GetComponent<PlayerHealth> ();
+	}
+
+	void Update ()
+	{
+		if (health.currentHealth < health.maxHealth) {
+			flameColor.g = 0.25f;
+			particles.emissionRate = woundedRate;
+		} else {
+			particles.startSize = fullHealthSize;
+			flameColor.g = 0.75f;
+			particles.emissionRate = fullHealthRate;
+		}
+		if (health.regenerate && health.currentHealth < health.maxHealth) {
+			flameColor.g = 0.75f;
+			flameColor.r = 0;
+			particles.startSize = fullHealthSize * (health.currentHealth / (health.maxHealth + 1));
+		} else {
+			flameColor.r = 1;
+		}
+		particles.startColor = flameColor;
+	}
+}
