7a550c4448c6c38462439d0368a236ae3dcfe358
41407 <jimi.welling@cs.helsinki.fi>
2015-10-04 17:48:45 +0300

    Fix ground object to follow player

---
+++ Assets/Scripts/Transform/MoveWithPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveWithPlayer : MonoBehaviour
+{
+	public GameObject player;
+	public float repeatRate = 10.0f;
+
+	void Update ()
+	{
+		Vector3 deltaPosition = transform.position - player.transform.position; 
+		if (Mathf.Abs (transform.position.x - player.transform.position.x) > repeatRate) {
+			transform.Translate (new Vector2 (-deltaPosition.x, 0));
+		}
+		
+		if (Mathf.Abs (transform.position.y - player.transform.position.y) > repeatRate) {
+			transform.Translate (new Vector2 (0, -deltaPosition.y));
+		}
+	}
+}
