5afc0da6feba03e043495019eec86b7b5615132f
41407 <jimi.welling@cs.helsinki.fi>
2015-10-05 00:18:04 +0300

    Fix player firing sound, pitch modulation script

---
+++ Assets/Scripts/SFX/RandomAudioClip.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomAudioClip : MonoBehaviour
+{
+	public AudioClip[] clips;
+	public float pitchMin = 1f;
+	public float pitchMax = 1f;
+
+	void OnEnable ()
+	{
+		gameObject.GetComponent<AudioSource> ().clip = clips [Random.Range (0, clips.Length)];
+		gameObject.GetComponent<AudioSource> ().Play ();
+		gameObject.GetComponent<AudioSource> ().pitch = Random.Range (pitchMin, pitchMax);
+	}
+}
