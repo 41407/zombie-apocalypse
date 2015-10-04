5455affc93da9e6c8d6325bd560e433e7b857c26
41407 <jimi.welling@cs.helsinki.fi>
2015-10-04 19:29:11 +0300

    Randomly choose grass textures

---
+++ Assets/Scripts/Actor/RandomTexture.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomTexture : MonoBehaviour {
+
+	public Texture2D[] textures;
+
+	void OnEnable () {
+		gameObject.GetComponent<Renderer>().material.mainTexture = textures[Random.Range(0, textures.Length)];
+	}
+}
