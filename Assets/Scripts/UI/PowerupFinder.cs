85f507e659f930841a3b6c1c4db2859d013c5868
41407 <jimi.welling@cs.helsinki.fi>
2015-10-04 23:34:22 +0300

    Add Powerup Finder

---
+++ Assets/Scripts/UI/PowerupFinder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupFinder : MonoBehaviour
+{
+
+	public GameObject powerUp;
+
+	void Update ()
+	{
+		if (powerUp) {
+			transform.LookAt (powerUp.transform.position);
+		} else {
+			powerUp = GameObject.FindGameObjectWithTag ("PowerUp");
+		}
+	}
+}
