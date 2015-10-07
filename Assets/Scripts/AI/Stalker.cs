3651e109d907c0e28fccad1d5a000060cbba5849
41407 <jimi.welling@cs.helsinki.fi>
2015-10-07 13:38:48 +0300

    Made Stalker more fair and maybe a bit more efficient

--- Assets/Scripts/AI/Stalker.cs
+++ Assets/Scripts/AI/Stalker.cs
@@ -7,20 +7,38 @@ public class Stalker : MonoBehaviour
 	public bool onCooldown = false;
 	public float cooldownTime = 2.0f;
 	public string targetTag;
+	private CircleCollider2D trigger;
+
+	void OnEnable ()
+	{
+		if (!trigger) {
+			trigger = gameObject.GetComponent<CircleCollider2D> ();
+		}
+	}
+
+	void OnBecameVisible ()
+	{
+		trigger.enabled = true;
+	}
+	
+	void OnBecameInvisible ()
+	{
+		trigger.enabled = false;
+	}
 
 	void OnTriggerEnter2D (Collider2D col)
 	{
 		if (!onCooldown && targetTag.Contains (col.tag)) {
-			Turn (col.gameObject);
+			Turn (col.gameObject.transform.position);
 			Charge ();
 			onCooldown = true;
 			Invoke ("CancelCooldown", cooldownTime);
 		}
 	}
 
-	private void Turn (GameObject towards)
+	private void Turn (Vector3 targetPosition)
 	{
-		Vector3 relativeLookDirection = transform.position - towards.transform.position;
+		Vector3 relativeLookDirection = transform.position - targetPosition;
 		float angle = Mathf.Atan2 (relativeLookDirection.y, relativeLookDirection.x) * Mathf.Rad2Deg + 90;
 		transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
 	}
