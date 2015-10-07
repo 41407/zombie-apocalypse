2d11297438b58f32f2292b8ae2260d3db49556c8
41407 <jimi.welling@cs.helsinki.fi>
2015-10-07 16:09:15 +0300

    Add medium daeth decal

--- Assets/Scripts/Game Controller/GroundGenerator.cs
+++ Assets/Scripts/Game Controller/GroundGenerator.cs
@@ -11,7 +11,6 @@ public class GroundGenerator : MonoBehaviour
 	public int numberOfGroundObjects = 1;
 	public int numberVariance = 5;
 	public float featureDistance = 25f;
-	private int zLayer = 1;
 
 	void Update ()
 	{
@@ -31,11 +30,6 @@ public class GroundGenerator : MonoBehaviour
 		Vector2 playerPosition = player.transform.position;
 		for (int i = 0; i < numberOfNewObjects; i++) {
 			Vector3 featurePosition = (Vector3)playerPosition + (Vector3)direction * featureDistance + transform.position;
-			//featurePosition.z -= zLayer / 100f;
-			//zLayer++;
-			//if (zLayer > 16) {
-			//	zLayer = 0;
-			//}
 			direction = Quaternion.AngleAxis (angleBetweenEach, Vector3.forward) * direction;
 			Factory.create.ByReference (groundFeature, featurePosition, Quaternion.AngleAxis (Random.Range (0, 360), Vector3.forward));
 		}
---
+++ Assets/Scripts/Transform/RandomScale.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomScale : MonoBehaviour {
+
+	public float min = 1;
+	public float max = 1;
+
+	void OnEnable() {
+		transform.localScale = Vector3.one * Random.Range (min, max);
+	}
+}
