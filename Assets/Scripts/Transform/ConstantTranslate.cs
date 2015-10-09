4c221ad9541741614a190c85f7bc196c93c2bec1
41407 <jimi.welling@cs.helsinki.fi>
2015-10-10 00:49:22 +0300

    Add some kind of a splash screen / main menu

---
+++ Assets/Scripts/Transform/ConstantTranslate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantTranslate : MonoBehaviour
+{
+
+	public Vector3 translation;
+
+	void Update ()
+	{
+		transform.Translate (translation * Time.deltaTime);
+	}
+}
