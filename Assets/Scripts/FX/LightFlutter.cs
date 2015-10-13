a69056a84ac32c720e4622de85a59ed5696f8976
41407 <jimi.welling@cs.helsinki.fi>
2015-10-13 23:39:24 +0300

    Make explosive enemy light flutter, fix explosion

---
+++ Assets/Scripts/FX/LightFlutter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlutter : MonoBehaviour
+{
+
+	private Light source;
+	public float min = 0;
+	public float max = 4;
+	private float target;
+	public float rate = 0.25f;
+	public float lerp = 0.4f;
+
+	void Awake ()
+	{
+		source = GetComponent<Light> ();
+	}
+
+	void OnEnable ()
+	{
+		InvokeRepeating ("Flutter", rate, rate);
+	}
+
+	void OnDisable ()
+	{
+		CancelInvoke ();
+	}
+
+	void Update ()
+	{
+		source.intensity = Mathf.Lerp (source.intensity, target, lerp);
+	}
+
+	void Flutter ()
+	{
+		target = Random.Range (min, max);
+	}
+}
